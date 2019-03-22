6f1cf6cb18710e4e3624c1bf0e5c2503bc41bdd8
ctelloordonez <ctelloordonez@users.noreply.github.com>
2019-03-22 06:56:11 +0100

    Heap (Pathfinding) - FIXED

--- InnoViralProject/InnoViralProject/Assets/Scripts/PathFinding/GridPathFinding.cs
+++ InnoViralProject/InnoViralProject/Assets/Scripts/PathFinding/GridPathFinding.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class GridPathFinding : MonoBehaviour
 {
+    public bool onlyDisplayPathGizmos;
     public LayerMask unwalkableMask;
     public Vector2 gridWorldSize;
     public float nodeRadius;
@@ -19,6 +20,14 @@ public class GridPathFinding : MonoBehaviour
         gridSizeY = Mathf.RoundToInt(gridWorldSize.y / nodeDiameter);
         CreateGrid();
 
+    }
+
+    public int MaxSize
+    {
+        get
+        {
+            return gridSizeX * gridSizeY;
+        }
     }
 
     private void CreateGrid()
@@ -78,21 +87,36 @@ public class GridPathFinding : MonoBehaviour
     {
         Gizmos.DrawWireCube(transform.position.normalized, new Vector3(1, gridWorldSize.y, gridWorldSize.x));
 
-        if ( grid != null)
+        if (onlyDisplayPathGizmos)
         {
-            foreach (Node node in grid)
+            if (path != null)
             {
-                Gizmos.color = (node.walkable) ? Color.white : Color.red;
-
-                if(path != null)
+                foreach (Node node in path)
                 {
-                    if (path.Contains(node))
-                    {
-                        Gizmos.color = Color.black;
-                    }
+                    Gizmos.color = Color.black;
+                    Gizmos.DrawCube(node.worldPosition, Vector3.one * (nodeDiameter - 0.1f));
                 }
+            }
+        }
 
-                Gizmos.DrawCube(node.worldPosition, Vector3.one * (nodeDiameter - 0.1f));
+        else
+        {
+            if (grid != null)
+            {
+                foreach (Node node in grid)
+                {
+                    Gizmos.color = (node.walkable) ? Color.white : Color.red;
+
+                    if (path != null)
+                    {
+                        if (path.Contains(node))
+                        {
+                            Gizmos.color = Color.black;
+                        }
+                    }
+
+                    Gizmos.DrawCube(node.worldPosition, Vector3.one * (nodeDiameter - 0.1f));
+                }
             }
         }
     }
--- InnoViralProject/InnoViralProject/Assets/Scripts/PathFinding/Pathfinding.cs
+++ InnoViralProject/InnoViralProject/Assets/Scripts/PathFinding/Pathfinding.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Diagnostics;
 
 public class Pathfinding : MonoBehaviour
 {
@@ -14,7 +15,10 @@ public class Pathfinding : MonoBehaviour
 
     private void Update()
     {
-        FindPath(seeker.position, target.position);
+        if (Input.GetButtonDown("Jump"))
+        {
+            FindPath(seeker.position, target.position);
+        }
     }
 
     void FindPath(Vector3 startPosition, Vector3 targetPosition)
@@ -22,25 +26,13 @@ public class Pathfinding : MonoBehaviour
         Node startNode = grid.NodeFromWorldPoint(startPosition);
         Node targetNode = grid.NodeFromWorldPoint(targetPosition);
 
-        List<Node> openSet = new List<Node>();
+        Heap<Node> openSet = new Heap<Node>(grid.MaxSize);
         HashSet<Node> closedSet = new HashSet<Node>();
         openSet.Add(startNode);
 
         while (openSet.Count > 0)
         {
-            Node currentNode = openSet[0];
-            for (int i = 1; i < openSet.Count; i++)
-            {
-                if (openSet[i].fCost < currentNode.fCost || openSet[i].fCost == currentNode.fCost)
-                {
-                    if (openSet[i].hCost < currentNode.hCost)
-                    {
-                        currentNode = openSet[i];
-                    }
-                }
-            }
-
-            openSet.Remove(currentNode);
+            Node currentNode = openSet.RemoveFirst();
             closedSet.Add(currentNode);
 
             if (currentNode == targetNode)
@@ -64,6 +56,11 @@ public class Pathfinding : MonoBehaviour
 
                     if (!openSet.Contains(neighbour))
                         openSet.Add(neighbour);
+
+                    else
+                    {
+                        //openSet.UpdateItem(neighbour);
+                    }
                 }
             }
         } 
