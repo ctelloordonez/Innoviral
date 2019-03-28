ac12e6265b1780bb03872d37b18807bd4350dc0c
ctelloordonez <ctelloordonez@users.noreply.github.com>
2019-03-28 14:11:17 +0100

    Smooth Path (in Progress)

--- InnoViralProject/InnoViralProject/Assets/Scripts/PathFinding/Unit.cs
+++ InnoViralProject/InnoViralProject/Assets/Scripts/PathFinding/Unit.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class Unit : MonoBehaviour
 {
+    const float minPathUpdateTime = .2f;
+    const float pathUpdateMoveThreshold = .5f;
+    
     public Transform target;
     public float speed = 2.5f;
+    public float turnDistance = 1f;
     public float turnSpeed = 2.5f;
 
-    Vector3[] path;
+    Path path;
     int targetIndex;
 
     private void Start()
@@ -24,18 +28,18 @@ public class Unit : MonoBehaviour
         }
     }*/
 
-    public void OnPathFound(Vector3[] newPath, bool pathSuccessful)
+    public void OnPathFound(Vector3[] waypoints, bool pathSuccessful)
     {
         if (pathSuccessful)
         {
-            path = newPath;
+            path = new Path(waypoints, transform.position, turnDistance);
             targetIndex = 0;
             StopCoroutine("FollowPath");
             StartCoroutine("FollowPath");
         }
     }
 
-    IEnumerator FollowPath()
+    /*IEnumerator FollowPath()
     {
         Vector3 currentWaypoint = path[0];
         while (true)
@@ -57,27 +61,78 @@ public class Unit : MonoBehaviour
             //transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, turnSpeed * Time.deltaTime);      // Rotation
             yield return null;
         }
+    }*/
+
+    IEnumerator FollowPath()
+    {
+        while (true)
+        {
+            yield return null;
+        }
     }
+
+    /*IEnumerator UpdatePath()
+    {
+        if(Time.timeSinceLevelLoad < .3f)
+        {
+            yield return new WaitForSeconds(.3f);
+        }
+        PathRequestManager.RequestPath(transform.position, target.position, OnPathFound);
+
+        float sqrMoveThreshold = pathUpdateMoveThreshold * pathUpdateMoveThreshold;
+        Vector3 targetPositionOld = target.position;
+
+        while (true)
+        {
+            yield return new WaitForSeconds(minPathUpdateTime);
+            if ((target.position - targetPositionOld).sqrMagnitude > sqrMoveThreshold)
+            {
+                PathRequestManager.RequestPath(transform.position, target.position, OnPathFound);
+                targetPositionOld = target.position;
+            }
+        }
+
+    }
+
+    IEnumerator FollowPath()
+    {
+        bool followingPath = true;
+        int pathIndex = 0;
+        transform.LookAt(path.lookPoints[0]);
+
+        float speedPercent = 1;
+
+        while (followingPath)
+        {
+            Vector2 position2D = new Vector2(transform.position.z, transform.position.y);
+            while (path.turnBoundaries[pathIndex].HasCrossedLine(position2D))
+            {
+                if(pathIndex == path.finishLineIndex)
+                {
+                    followingPath = false;
+                    break;
+                }
+
+                else
+                {
+                    pathIndex++;
+                }
+            }
+
+            if (followingPath)
+            {
+                
+            }
+
+            yield return null;
+        }
+    }*/
 
     public void OnDrawGizmos()
     {
         if (path != null)
         {
-            for (int i = targetIndex; i < path.Length; i++)
-            {
-                Gizmos.color = Color.black;
-                Gizmos.DrawCube(path[i], Vector3.one);
-
-                if (i == targetIndex)
-                {
-                    Gizmos.DrawLine(transform.position, path[i]);
-                }
-
-                else
-                {
-                    Gizmos.DrawLine(path[i-1], path[i]);
-                }
-            }
+            path.DrawWithGizmos();
         }
     }
 }
