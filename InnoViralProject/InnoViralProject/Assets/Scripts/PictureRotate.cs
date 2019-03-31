3118305e499f8aed0602942bbf410dd7ef199425
shamil1233 <43807064+shamil1233@users.noreply.github.com>
2019-03-31 16:00:26 +0200

    Inventory added

--- InnoViralProject/InnoViralProject/Assets/Scripts/PictureRotate.cs
+++ InnoViralProject/InnoViralProject/Assets/Scripts/PictureRotate.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PictureRotate : MonoBehaviour
-{
+{ 
     private void OnMouseDown()
     {
         if (!GameControl.youWin)
         {
             transform.Rotate(0f, 0f, 90f);
         }
-    }
+    } 
 }
