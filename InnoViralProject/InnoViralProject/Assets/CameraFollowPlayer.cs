3a1ed4ab7c0a5f5140ff10848d0f45e139e88d9c
shamil1233 <43807064+shamil1233@users.noreply.github.com>
2019-04-04 15:22:29 +0200

    camare

---
+++ InnoViralProject/InnoViralProject/Assets/CameraFollowPlayer.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class CameraFollowPlayer : MonoBehaviour
+{
+    public Transform target;
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset;
+
+    private void Update()
+    {
+        transform.position = target.position + offset;
+
+    }
+}
